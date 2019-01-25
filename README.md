11cf2da9c3e5183cc493ddc6384ebd6277aff259
PanJinYi <657678263@qq.com>
2019-01-25 15:37:15 +0800

    创建脚本文件xiaofeiniu.sql

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # xiaofeiniu-api
+# xiaofeiniu-api
 小肥牛扫码点餐系统-数据API子系统
---
+++ xiaofeiniu.sql
@@ -0,0 +1,4 @@
+SET NAMES UTF8;
+DROP DATABASE IF EXISTS xiaofeiniu;
+CREATE DATABASE xiaofeiniu CHARSET=UTF8;
+USE xiaofeiniu; 
